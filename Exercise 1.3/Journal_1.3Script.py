1b02bfd206e0d97832f651fe731b9a83993c6030
Marvel2410 <139808890+Marvel2410@users.noreply.github.com>
2024-02-27 01:14:25 -0800

    Journal update

---
+++ Exercise 1.3/Journal_1.3Script.py
@@ -0,0 +1,16 @@
+destinations = ["Paris", "Netherlands", "New York", "Oregon", "Italy"]
+
+user_destination = input("Where do you want to travel?")
+
+if user_destination == "Paris":
+    print("Enjoy your vacation in Paris!")
+elif user_destination == "Netherlands":
+    print("Enjoy your vacation in the Netherlands!")
+elif user_destination == "New York":
+    print("Enjoy your vacation in New York!")
+elif user_destination == "Oregon":
+    print("Enjoy your vacation in Oregon!")
+elif user_destination == "Italy":
+    print("Enjoy your vacation in Italy!")
+else:
+    print("That destination is currently unavailable.")
